c9beedd61a94a347fa50c9d7942fdc619f7b36b3
lazevedo <lazevedo@STFSAOC045860-L>
2023-03-10 16:37:11 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp_BuscaBinaria/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp_BuscaBinaria
+{
+    class Program
+    {
+
+        public static bool buscaBinaria(int[] relacaoIdades, int num)
+        {
+            int esq = 0;
+            int dir = relacaoIdades.Length - 1;
+            while (esq <= dir)
+            {
+                int meio = (esq + dir) / 2;
+                if (relacaoIdades[meio] == num) return true;
+                if (relacaoIdades[meio] < num) esq = meio + 1;
+                else dir = meio - 1;
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] idadesRelacao = new int[] { 14, 16, 17, 19 };
+
+            for (int i = 0; i < idadesRelacao.Length; i++)
+            {
+                Console.WriteLine("Idades são {0}", idadesRelacao[i]);
+            }
+
+            bool resultado = buscaBinaria(idadesRelacao, 20);
+
+
+            Console.WriteLine("Resultado {0}", resultado);
+
+            Console.ReadLine();
+        }
+    }
+}
